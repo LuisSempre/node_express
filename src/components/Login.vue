71aa68f5c83da4375fdc67d80ec30037ce011776
LuisSempre <luis.silva@viannasempre.com.br>
2023-08-24 02:38:17 -0300

    feat: add login

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="login">
+      <input v-model="username" type="text" placeholder="Username" />
+      <input v-model="password" type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    login() {
+      // Simulate authentication (replace this with your actual login logic)
+      if (this.username === 'user' && this.password === 'password') {
+        this.$router.push('/about'); // Redirect to the main app page
+      } else {
+        alert('Invalid credentials');
+      }
+    }
+  }
+};
+</script>
+
---
+++ src/views/AboutView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+		    <button @click="logout">Logout</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      // Coloque aqui a lógica de logout
+      // Por exemplo, redirecionar para a página de login
+      this.$router.push('/');
+    }
+  }
+};
+</script>
